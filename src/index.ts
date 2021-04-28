fd4e8e6c4cad70a80c739ad5d35a7076d2e14dbd
yaegassy <yaegassy@users.noreply.github.com>
2021-04-28 17:10:30 +0900

    feat: add hover feature

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export async function activate(): Promise<void> {
-  // noop
+import { ExtensionContext, languages, workspace } from 'coc.nvim';
+
+import { BladeHoverProvider } from './hover';
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  const extensionConfig = workspace.getConfiguration('bladeSnippets');
+  const isEnable = extensionConfig.get<boolean>('enable', true);
+  if (!isEnable) return;
+
+  context.subscriptions.push(languages.registerHoverProvider(['blade'], new BladeHoverProvider(context)));
 }
